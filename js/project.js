8a6e02398f57fbd2a595c10e28752bdbae5351c8
Drliehuo <92857893+Drliehuo@users.noreply.github.com>
2023-01-27 18:36:11 +0800

    Create project.js

---
+++ js/project.js
@@ -0,0 +1,4 @@
+document.write("<h4>Project</h4>");
+document.write("<ol class=\'list-unstyled\'>");
+document.write("<li><a href=\'https://inetpub.cn/jwp7.html\' title=\'jw player 7\'>JWP7</a></li>");
+document.write("</ol>");
