7bb7134fcda7ff931d1de0b940ae0c4ba299b12c
Drliehuo <92857893+Drliehuo@users.noreply.github.com>
2023-01-30 21:11:55 +0800

    Update project.js

--- js/project.js
+++ js/project.js
@@ -1,4 +1,5 @@
 document.write("<h4>Project</h4>");
 document.write("<ol class=\'list-unstyled\'>");
 document.write("<li><a href=\'https://inetpub.cn/jwp7.html\' title=\'jw player 7\'>JWP7</a></li>");
+document.write("<li><a href=\'https://inetpub.cn/project/lighttp.html\' title=\'lighttp\'>lighttp</a></li>");
 document.write("</ol>");
