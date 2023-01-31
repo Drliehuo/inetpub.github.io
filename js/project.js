35f3576f90f6f5df3dbae91674a0795cadd443b0
Drliehuo <92857893+Drliehuo@users.noreply.github.com>
2023-01-31 13:50:42 +0800

    Update project.js

--- js/project.js
+++ js/project.js
@@ -1,6 +1,6 @@
 document.write("<h4>Project</h4>");
 document.write("<ol class=\'list-unstyled\'>");
-document.write("<li><a href=\'https://inetpub.cn/ckeditor.html\' title=\'ckeditor 4\'>ckeditor4</a></li>");
+document.write("<li><a href=\'https://inetpub.cn/project/ckeditor.html\' title=\'ckeditor 4\'>ckeditor4</a></li>");
 document.write("<li><a href=\'https://inetpub.cn/jwp7.html\' title=\'jw player 7\'>JWP7</a></li>");
 document.write("<li><a href=\'https://inetpub.cn/project/lighttp.html\' title=\'lighttp\'>lighttp</a></li>");
 document.write("</ol>");
