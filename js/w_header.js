bd3bb26a09644f7a17a4486fa01043d808d59d2b
Drliehuo <92857893+Drliehuo@users.noreply.github.com>
2023-02-05 03:19:13 +0800

    Update w_header.js

--- js/w_header.js
+++ js/w_header.js
@@ -1,4 +1,5 @@
 document.write("<nav class=\'blog-nav\'>");
 document.write("<a class=\'blog-nav-item active\' href=\'https://inetpub.cn/\'>首页</a>");
 document.write("<a class=\'blog-nav-item\' href=\'https://inetpub.cn/project\'>项目</a>");
+document.write("<a class=\'blog-nav-item\' href=\'https://inetpub.cn/notes\'>笔记</a>");
 document.write("</nav>");
