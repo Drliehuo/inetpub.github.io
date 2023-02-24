8ef1524b7ef61659537cce2508e95b5627b96af1
Drliehuo <92857893+Drliehuo@users.noreply.github.com>
2023-02-24 13:33:12 +0800

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -1,2 +1,2 @@
-document.write("<p>&copy; 2014-2023 <a href=\'https://inetpub.cn/\'>Inetpub</a> by <a href=\'#\'>@Mr.Chen</a>.</p>");
+document.write("<p>&copy; 2014-2023 <a href=\'https://inetpub.cn/\'>Inetpub</a> by <a href=\'https://inetpub.cn/\'>@Mr.Chen</a> Powered By <a href=\'https://github.com/Drliehuo/lighttp\'>Lighttp</a>.</p>");
 document.write("<p><a href=\'#\'>Back to top</a></p>");
